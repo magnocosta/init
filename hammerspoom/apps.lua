52d6d922662d7c4d47fd299b573772922f12d0c5
Magno Costa <magno.costa@m4u.com.br>
2020-06-16 12:39:03 -0300

    Add screen split by windows

--- hammerspoom/apps.lua
+++ hammerspoom/apps.lua
@@ -1,37 +1,70 @@
+local alert  = require("hs.alert")
+local layout = require("hs.layout")
 
-local app_manager = { "cmd", "alt", "ctrl" }
+hs.application.enableSpotlightForNameSearches(true)
+
+local app_manager = { "cmd", "alt" }
+
+-------------------------------------
+-- Window layouts
+-------------------------------------
+local centerScreen = hs.screen{x=0,y=0}
+local rightScreen  = hs.screen{x=1,y=0}
+
+local twoScreenLayout = {
+ {"Iterm",         nil, centerScreen, layout.maximized, nil, nil},
+ {"Google Chrome", nil, centerScreen, layout.maximized, nil, nil},
+
+ {"WhatsApp",      nil, rightScreen, layout.maximized, nil, nil},
+ {"Slack",         nil, rightScreen, layout.maximized, nil, nil},
+ {"Telegram",      nil, rightScreen, layout.maximized, nil, nil},
+
+ {"Sublime Text", nil, rightScreen,  layout.maximized, nil, nil},
+ {"Spotify",      nil, rightscreen,  layout.maximized, nil, nil}
+}
+
+function switchLayout()
+  local numScreens = 2
+  if numScreens == 1 then
+    myLayout = twoScreenLayout
+  elseif numScreens == 2 then
+    myLayout = twoScreenLayout
+  elseif numScreens == 3 then
+    myLayout = {}
+  end
+  layout.apply(myLayout)
+end
+
+hs.hotkey.bind(app_manager, "s", function()
+  switchLayout()
+end)
+
 
 local shortcuts = {
-  { shortcut = "Q", command = "Spark" },
+  { shortcut = "]", command = "iTerm" },
+  { shortcut = "[", command = "Google Chrome" },
+  
   { shortcut = "W", command = "WhatsApp" },
   { shortcut = "E", command = "Slack" },
-  { shortcut = "R", command = "MIcrosoft Teams" },
-  { shortcut = "T", command = "Discord" },
+  { shortcut = "R", command = "Telegram" },
 
-  { shortcut = "P", command = "iTerm" },
   { shortcut = "O", command = "Sublime Text" },
-  { shortcut = "I", command = "Postman" },
-  { shortcut = "U", command = "Dash" },
-
-  { shortcut = "M", command = "Google Chrome" },
-  { shortcut = "N", command = "Firefox" },
-  { shortcut = "B", command = "Safari" },
-
   { shortcut = "A", command = "Spotify" },
-  { shortcut = "S", command = "TweetDeck" }
+  { shortcut = "R", command = "TweetDeck" },
+  { shortcut = "Z", command = "TogglDesktop" }
 }
 
 local shortcuts_help = ""
 
 for k, v in ipairs(shortcuts) do
   hs.hotkey.bind(app_manager, v.shortcut, function()
-    hs.application.launchOrFocus(v.command)
+   app = hs.application.launchOrFocus(v.command)
   end)
   shortcuts_help = shortcuts_help .. v.shortcut .. " - " .. v.command .. "\n"
 end
 
 hs.hotkey.bind(app_manager, "/", function()
-  hs.alert.show(shortcuts_help)
+  alert.show(shortcuts_help)
 end)
 
 hs.hotkey.bind(app_manager, "Z", function()
@@ -40,21 +73,14 @@ hs.hotkey.bind(app_manager, "Z", function()
   end
 end)
 
-hs.hotkey.bind(app_manager, "X", function()
-  for k, v in ipairs(shortcuts) do
-    hs.alert.show("try full: " .. v.command)
-    app = hs.application.get(v.command)
-    window = app:mainWindow()
-    window:setFullScreen(true)
-  end
-end)
-
 hs.hotkey.bind(app_manager, "C", function()
   for k, v in ipairs(shortcuts) do
-    hs.alert.show("try kill: " .. v.command)
     app = hs.application.get(v.command)
-    app:kill9()
-    hs.alert.show("Kill: " .. v.command)
+    if app == nil then
+      alert.show("Hammerspoon can't close app "..v.command)
+    else
+      app:kill9()
+    end
   end
 end)
 
--- hammerspoom/windows.lua
+++ hammerspoom/windows.lua
@@ -1,28 +1,84 @@
-spaces = require("hs._asm.undocumented.spaces")
+local spaces        = require("hs._asm.undocumented.spaces")
+local grid          = require("hs.grid")
+local window        = require("hs.window")
+local screen        = require("hs.screen")
+local hotkey        = require("hs.hotkey")
+local application   = require("hs.application")
+local alert         = require("hs.alert")
+local grid          = require("hs.grid")
 
-local window_manager = { "cmd", "alt", "ctrl" }
-hs.window.animationDuration = 0
+local window_manager = { "cmd", "alt" }
+local window_mana    = { "cmd", "alt", "ctrl" }
 
-hs.hotkey.bind(window_manager, "G", function()
-  hs.grid.show()
+window.animationDuration = 0
+
+hotkey.bind(window_manager, "G", function()
+  splitScreenByCurrentWindows()
 end)
 
-hs.hotkey.bind(window_manager, "F", function()
-  hs.window.focusedWindow():toggleFullScreen()
+hotkey.bind(window_manager, "F", function()
+  window.focusedWindow():maximize()
 end)
 
-hs.hotkey.bind(window_manager, "L", function()
-  screen = hs.screen('1,0')
-  window = hs.window.focusedWindow()
-  window:setFullScreen(false)
-  window:moveToScreen(screen)
+hotkey.bind(window_manager, "L", function()
+  moveFocusedWindowToScreen('1,0')
 end)
 
-hs.hotkey.bind(window_manager, "H", function()
-  screen = hs.screen('0,0')
-  window = hs.window.focusedWindow()
-  is_fullscreen = window:isFullScreen()
-  window:setFullScreen(false)
-  window:moveToScreen(screen)
+hotkey.bind(window_manager, "H", function()
+  moveFocusedWindowToScreen('0,0')
 end)
 
+hotkey.bind(window_mana, "L", function()
+  moveAllAppsToScreen('1,0')
+end)
+
+hotkey.bind(window_mana, "H", function()
+  moveAllAppsToScreen('0,0')
+end)
+
+function moveAllAppsToScreen(target)
+  local currentScreen = screen(target)
+  for _,app in ipairs(application.runningApplications()) do
+    for _,w in ipairs(app:allWindows()) do 
+      w:moveToScreen(currentScreen)
+    end
+  end
+end
+
+function moveFocusedWindowToScreen(target)
+  local currentScreen = screen(target)
+  local currentWindow = window.focusedWindow()
+  currentWindow:moveToScreen(currentScreen)
+end
+
+function getWindowsByCurrentScreen()
+
+end
+
+function splitScreenByCurrentWindows()
+  local currentWindow = window.focusedWindow()
+  local currentScreen = currentWindow:screen()
+  local count  = 0
+  local windowsByCurrentScreen = {}
+
+  for _,app in ipairs(application.runningApplications()) do
+    for _,w in ipairs(app:visibleWindows()) do 
+      if w:screen() == currentScreen and  w:title() ~= "" then
+        count = count + 1
+        windowsByCurrentScreen[count] = w
+        -- alert.show(w:title())
+      end
+    end
+  end
+
+  -- alert.show(count)
+
+  grid.setGrid(count..'x1')
+  count = 0
+  for _,w in ipairs(windowsByCurrentScreen) do 
+    local geo = hs.geometry:point('0,0')
+    grid.set(w, {x=count, y=1, w=1, h=1}, currentScreen)
+    count = count + 1
+  end
+
+end
