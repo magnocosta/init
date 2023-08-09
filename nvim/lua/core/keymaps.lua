d5fbff66bd868f9d8784a63c44d47c816df2e638
Magno Costa <magno@route.com>
2023-08-09 10:28:09 -0300

    Add some new keymaps

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -25,15 +25,23 @@ keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
+keymap("n", "<C-d>", "<C-d>zz", opts)
+keymap("n", "<C-u>", "<C-u>zz", opts)
+keymap("n", "n", "nzz", opts)
+keymap("n", "N", "Nzz", opts)
+
 -- Remap L and H
 keymap("n", "H", "0", opts)
 keymap("n", "L", "$", opts)
 
+-- Save the copy context
+keymap("x", "<leader>p", "\"_dP", opts)
+
 -- Close all buffers
-keymap("n", "bq", "<cmd>:%bd|e#<cr>", opts)
+keymap("n", "<leader>bq", "<cmd>:%bd|e#<cr>", opts)
 
 -- Remap tabs
-keymap("n", "t", "<cmd>tabnew<cr>", opts)
+keymap("n", "<leader>t", "<cmd>tabnew<cr>", opts)
 
 -- Nvim Tree
 keymap("n", "<c-n>", ":NvimTreeToggle<CR>", opts)
@@ -41,18 +49,17 @@ keymap("n", "<c-f>", ":NvimTreeFindFile<CR>", opts)
 
 -- Telescope
 keymap("n", "<c-p>", "<cmd>Telescope find_files<cr>", opts)
-keymap("n", "ff", "<cmd>Telescope find_files<cr>", opts)
-keymap("n", "fg", "<cmd>Telescope live_grep<cr>", opts)
-keymap("n", "fb", "<cmd>Telescope buffers<cr>", opts)
-keymap("n", "fb", "<cmd>Telescope buffers<cr>", opts)
-keymap("n", "fv", "<cmd>Telescope git_branches<cr>", opts)
+keymap("n", "<leader>ff", "<cmd>Telescope find_files<cr>", opts)
+keymap("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", opts)
+keymap("n", "<leader>fb", "<cmd>Telescope buffers<cr>", opts)
+keymap("n", "<leader>fv", "<cmd>Telescope git_branches<cr>", opts)
 
 -- Fugitive
-keymap("n", "gd", "<cmd>:Gvdiffsplit<cr>", opts)
-keymap("n", "gs", "<cmd>:Git<cr>", opts)
-keymap("n", "gb", "<cmd>:Git blame<cr>", opts)
-keymap("n", "gc", "<cmd>:Git commit<cr>", opts)
-keymap("n", "ge", "<cmd>:Gedit<cr>", opts)
-keymap("n", "gr", "<cmd>:Gread<cr>", opts)
-keymap("n", "gp", "<cmd>:G push<cr>", opts)
-keymap("n", "gl", "<cmd>:0Gclog<cr>", opts)
+keymap("n", "<leader>gd", "<cmd>:Gvdiffsplit<cr>", opts)
+keymap("n", "<leader>gs", "<cmd>:Git<cr>", opts)
+keymap("n", "<leader>gb", "<cmd>:Git blame<cr>", opts)
+keymap("n", "<leader>gc", "<cmd>:Git commit<cr>", opts)
+keymap("n", "<leader>ge", "<cmd>:Gedit<cr>", opts)
+keymap("n", "<leader>gr", "<cmd>:Gread<cr>", opts)
+keymap("n", "<leader>gp", "<cmd>:G push<cr>", opts)
+keymap("n", "<leader>gl", "<cmd>:0Gclog<cr>", opts)
