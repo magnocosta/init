873b5372ef86ec7d7ae0f78fbc3e01cf866dfc16
Magno Costa <magnocosta.br@gmail.com>
2023-10-02 13:08:50 -0300

    Upadate the indent-blankline plugin

--- nvim/lua/core/indent-blankline.lua
+++ nvim/lua/core/indent-blankline.lua
@@ -1,4 +1,4 @@
-local status_ok, indentBlankLine = pcall(require, "indent_blankline")
+local status_ok, indentBlankLine = pcall(require, "ibl")
 
 if not status_ok then
 	return
@@ -8,8 +8,8 @@ vim.opt.list = true
 vim.opt.listchars:append("space:⋅")
 -- vim.opt.listchars:append("eol:↴")
 
-indentBlankLine.setup({
-	space_char_blankline = " ",
-	show_current_context = true,
-	show_current_context_start = true,
-})
+-- indentBlankLine.setup({
+-- 	space_char_blankline = " ",
+-- 	show_current_context = true,
+-- 	show_current_context_start = true,
+-- })
