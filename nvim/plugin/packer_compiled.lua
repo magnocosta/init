2069e5284734b79ff508f3e19b7167c633a9aaa5
Magno Costa <magnocosta.br@gmail.com>
2023-10-02 13:10:03 -0300

    Update the dependencies

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -124,6 +124,11 @@ _G.packer_plugins = {
     path = "/Users/magnocosta/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
     url = "https://github.com/saadparwaiz1/cmp_luasnip"
   },
+  ["dart-vim-plugin"] = {
+    loaded = true,
+    path = "/Users/magnocosta/.local/share/nvim/site/pack/packer/start/dart-vim-plugin",
+    url = "https://github.com/dart-lang/dart-vim-plugin"
+  },
   ["dracula.nvim"] = {
     loaded = true,
     path = "/Users/magnocosta/.local/share/nvim/site/pack/packer/start/dracula.nvim",
