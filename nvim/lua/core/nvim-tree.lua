936db5f5d469deedeb1ecd2b3bc7aa6c49c42912
Magno Costa <magno@route.com>
2023-10-04 15:22:50 -0300

    Add new configs to nvim-tree

--- nvim/lua/core/nvim-tree.lua
+++ nvim/lua/core/nvim-tree.lua
@@ -6,11 +6,13 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
 require("nvim-tree").setup {
-  sync_root_with_cwd = true,
-  respect_buf_cwd = true,
+  filters = {
+    dotfiles = true,
+  },
   update_focused_file = {
     enable = true,
-    update_root = true
+    update_cwd = false,
+    ignore_list = {},
   },
   view = {
     side = "right",
@@ -18,13 +20,56 @@ require("nvim-tree").setup {
   },
   renderer = {
     group_empty = true,
+    indent_markers = {
+      enable = true,
+      icons = {
+        corner = "└ ",
+        edge = "│ ",
+        item = "│ ",
+        none = "  ",
+      },
+    },
     icons = {
+      webdev_colors = false,
+      show = {
+        file = false,
+        folder = true,
+        folder_arrow = false,
+        git = true
+      },
       glyphs = {
+        default = "",
+        symlink = "",
         folder = {
-          arrow_closed = "⏵",
-          arrow_open = "⏷",
+          arrow_closed = "",
+          arrow_open = "",
+          default = "",
+          open = "",
+          empty = "",
+          empty_open = "",
+          symlink = "",
+          symlink_open = "",
+        },
+        git = {
+          unstaged = "", -- 
+          staged = "",
+          unmerged = "",
+          renamed = "➜",
+          untracked = "",
+          deleted = "",
+          ignored = "◌",
         },
       },
+    },
+  },
+  diagnostics = {
+    enable = true,
+    show_on_dirs = false,
+    icons = {
+      hint = "",
+      info = "",
+      warning = "",
+      error = "",
     },
   },
 }
