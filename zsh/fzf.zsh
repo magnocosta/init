0b67c0e299aae9cc7c00be9972469e464e4978f3
Magno Costa <magnocosta.br@gmail.com>
2024-09-03 14:34:11 -0300

    Add file preview into FZF config

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,16 +1,14 @@
 export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border'
 export FZF_COMPLETION_TRIGGER='**'
+export FZF_CTRL_T_OPTS="
+ --walker-skip .git,node_modules,target
+ --preview 'bat --color=always --style=numbers --line-range=:500 {}'
+ --bind 'ctrl-/:change-preview-window(down|hidden|)'
+"
 
-# Setup fzf
-# ---------
 if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
   PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
 fi
 
-# Auto-completion
-# ---------------
-source "/usr/local/opt/fzf/shell/completion.zsh"
-
-# Key bindings
-# ------------
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+# source "/usr/local/opt/fzf/shell/completion.zsh"
+# source "/usr/local/opt/fzf/shell/key-bindings.zsh"
