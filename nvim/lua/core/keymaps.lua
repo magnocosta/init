b14844734d92fbb1122719f636959d3af0256fec
Magno Costa <magnocosta.br@gmail.com>
2024-10-11 19:57:58 -0300

    Add easy word replace (thanks theprimegen)

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -80,3 +80,5 @@ keymap("n", "<leader>gl", "<cmd>:0Gclog<cr>", opts)
 
 keymap("n", "<leader>gh", "<cmd>:diffget //2<cr>", opts)
 keymap("n", "<leader>gu", "<cmd>:diffget //3<cr>", opts)
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
