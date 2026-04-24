808b08a68c98964fc78150c980a33026fea98abf
Magno Costa <magnocosta.br@gmail.com>
2026-04-24 10:58:51 -0300

    Adds more apps

--- omarchy/install.sh
+++ omarchy/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo pacman -S --needed \
-  ack bat curl fzf git go jq libpq stow tmux tree zsh
+  ack bat curl fzf git go jq libpq stow tmux tree zsh eza
 
 sudo yay -S --needed \
   ghostty jqp tfenv tmuxinator
