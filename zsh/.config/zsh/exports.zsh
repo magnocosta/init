84b5c9af903a78d567dba3a4bee670e26d73358d
Magno Costa <magnocosta.br@gmail.com>
2026-05-21 11:03:36 -0300

    Adds XDG data as default for Tmux plugins

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -3,6 +3,7 @@ export DOTENV_PATH="$HOME/.init"
 export POSTMAN_PATH="$HOME/Work/tools/postman"
 export MANPAGER="sh -c 'sed -u -e \"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\" | bat -p -lman'"
 export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
 
 path=(
   "$HOME/.local/bin"
