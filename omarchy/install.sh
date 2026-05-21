be8af9f9b482602966a7f08206cf44a98261c06e
Magno Costa <magnocosta.br@gmail.com>
2026-05-20 23:12:29 -0300

    Adds asdf to manage enviroments

--- omarchy/install.sh
+++ omarchy/install.sh
@@ -4,4 +4,4 @@ sudo pacman -S --needed \
   ack bat curl fzf git go jq libpq stow tmux tree zsh eza syncthing wget
 
 sudo yay -S --needed \
-  ghostty jqp tfenv tmuxinator
+  asdf-vm ghostty jqp tfenv tmuxinator
