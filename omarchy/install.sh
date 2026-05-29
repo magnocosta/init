3da9cd2815f8c0ce0dbb3288db461505abb9b7ec
Magno Costa <magnocosta.br@gmail.com>
2026-05-29 17:46:49 -0300

    Adds Android Studios into Omarchy

--- omarchy/install.sh
+++ omarchy/install.sh
@@ -4,4 +4,4 @@ sudo pacman -S --needed \
   ack bat curl fzf git go jq libpq stow tmux tree zsh eza syncthing wget ninja cmake
 
 sudo yay -S --needed \
-  asdf-vm ghostty jqp tmuxinator
+  asdf-vm ghostty jqp tmuxinator android-studio
--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -4,8 +4,13 @@ export POSTMAN_PATH="$HOME/Work/tools/postman"
 export MANPAGER="sh -c 'sed -u -e \"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\" | bat -p -lman'"
 export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
 export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
+export ANDROID_HOME="$HOME/Android/Sdk"
+export ANDROID_AVD_HOME="$HOME/.config/.android/avd"
 
 path=(
   "$HOME/.local/bin"
+  "$ANDROID_HOME/emulator"
+  "$ANDROID_HOME/platform-tools"
+  "$ANDROID_HOME/cmdline-tools/latest/bin"
   $path
 )
